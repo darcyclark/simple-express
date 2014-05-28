7a7a314211a7928366c50ffb3d0a26a5733809ea
Darcy Clark <darcy@qualitysystems.com>
2014-05-29 00:23:30 +0200

    switched back to plain Express approach with added static goodness
    see /support route for trickiness

--- routes/support.js
+++ routes/support.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var dir = require('node-dir');
+var fm = require('front-matter');
+var jfm = require('json-front-matter');
 
 router.get('/', function(req, res) {
-  res.render('support/index', { title: 'Support Index' });
+  data = new Array();
+  dir.readFiles("./views/support/pages", {
+    match: /.jade/,
+    shortName: true
+  }, 
+  function(err, content, filename, next) {
+    var out = fm( content );
+    if (out.attributes.draft == true) { 
+      f = filename.substring(0, filename.lastIndexOf('.'))
+      data.push([ f, out.attributes.title, out.attributes.date ]);
+    };
+    next();
+  },
+  function(err, files){
+    if (err) throw err;
+    var column = 2; // sort by date
+    sorted = data.sort(function(x,y) { return x[column] < y[column] });
+    res.render('support/index', { data:sorted, title: 'Support Index' });
+  });
 });
 
 module.exports = router;
