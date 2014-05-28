c5d8e04d514690d5a4eb878b0382dfa3eefa7e89
Darcy Clark <darcy@qualitysystems.com>
2014-05-29 00:48:12 +0200

    updated docs

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ var support = require('./routes/support');
 app.use('/', routes);
 app.use('/support', support);
 
-// static page support
 // i.e. add pages under /support/pages without needing to add routes
 app.get('/support/pages*', semiStatic({
   folderPath: __dirname + '/views/support/pages',
